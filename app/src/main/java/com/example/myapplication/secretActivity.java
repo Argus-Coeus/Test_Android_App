2ac171a3995f39bdcc5d11b10229b97ca277fea5
Argus Ceous <argus@letmein.letmein>
2024-09-28 14:07:35 -0400

    A simple test app

---
+++ app/src/main/java/com/example/myapplication/secretActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class secretActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_secret);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent receivedIntent = getIntent();
+        String sharedIntent = receivedIntent.getStringExtra(Intent.EXTRA_TEXT);
+        if (sharedIntent != null){
+            TextView debug_text = findViewById(R.id.debug_text);
+            debug_text.setText(String.format("Shared : %s",sharedIntent));
+        }
+    }
+}
